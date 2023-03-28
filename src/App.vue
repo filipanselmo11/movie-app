8a0c167aad77e7bd8133a016e79bbdc87211c694
Fílip Anselmo <filipanselmoalvesdasilva870@gmail.com>
2023-03-28 14:53:24 -0300

    Add mais componentes

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <v-app>
     <v-main>
       <MovieAppBar/>
+      <MovieInput/>
     </v-main>
   </v-app>
 </template>
@@ -9,12 +10,14 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import MovieAppBar from './components/MovieAppBar.vue';
+import MovieInput from './components/MovieInput.vue';
 
 export default defineComponent({
   name: 'App',
 
   components: {
-    MovieAppBar
+    MovieAppBar,
+    MovieInput
 },
 
   data () {
--- src/components/MovieInput.vue
+++ src/components/MovieInput.vue
@@ -3,8 +3,31 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-    name: 'MovieInput'
+    name: 'MovieInput',
+    data() {
+        return {
+            loaded: false,
+            loading: false,
+        }
+    },
+
+    methods: {
+        search() {
+            this.loading = true;
+            setTimeout(() => {
+                this.loading = false;
+                this.loaded = true;
+            }, 2000);
+        }
+    },
 })
 </script>
 
-<template></template>
+<template>
+    <v-card class="mx-auto mt-10" color="grey" max-width="400">
+        <v-card-text>
+            <v-text-field :loading="loading" density="compact" variant="solo" label="Search Movies"
+                append-inner-icon="mdi-magnify" single-line hide-details @click:append-inner="search"></v-text-field>
+        </v-card-text>
+    </v-card>
+</template>
